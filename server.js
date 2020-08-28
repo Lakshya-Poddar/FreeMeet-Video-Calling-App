ed79123bbcc822530ea3c5a917e36d014bcbec62
Lakshya-Poddar <lakshyapoddar1604@gmail.com>
2020-08-28 13:35:41 +0530

    Audio Vedio start stop done

--- server.js
+++ server.js
@@ -17,10 +17,13 @@ app.get("/:room", (req, res) => {
 });
 
 io.on("connection", (socket) => {
-  socket.on("join-room", (roomId,userId) => {
+  socket.on("join-room", (roomId, userId) => {
     socket.join(roomId);
-    socket.to(roomId).broadcast.emit("user-connected",userId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on('message',message=>{
+      io.to(roomId).emit('createMessage',message)
+    })
   });
 });
 
-server.listen(3030);
+server.listen(3000);
